4690d0000c3e533a4c6b64ad98e8a65d80fdf871
moshenglei <moshenglei@icloud.com>
2019-10-26 11:10:25 +0800

    添加依赖

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var httpProxy = require('http-proxy');
+var compression = require('compression');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+var proxy = httpProxy.createProxyServer();
+
+app.use(express.static(__dirname + '/dist'));
+
+app.use(compression());
+app.use(cookieParser());
+
+app.all('/v1/*', function(req, res) {
+
+    const API = 'http://localhost:8182';
+
+    //代理
+    proxy.web(req, res, {
+        target: API,
+        changeOrigin: true
+    });
+});
+app.listen(9093);
